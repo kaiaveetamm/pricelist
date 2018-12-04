f3a8cc402eee63cff4b22a76f20268a351e9bf1c
kaiaveetamm <45009414+kaiaveetamm@users.noreply.github.com>
2018-12-04 16:46:39 +0200

    Add files via upload

---
+++ Kodutöö kinganr teisendaja/kinganumber.js
@@ -0,0 +1,9 @@
+//kinganumbri teisendaja ehk kirves!
+
+function myFunction() {
+    document.getElementById("result").innerHTML = "Sinu  US kinganumber on: ";
+}
+
+function LengthConverter(valNum) {
+  document.getElementById("outputInches").innerHTML=valNum*0.2;
+}
